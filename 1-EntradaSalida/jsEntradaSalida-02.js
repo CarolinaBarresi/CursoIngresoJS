4d095a13d746e1477711140bc6cc2092a195ff79
CarolinaBarresi <carolinabarresi@gmail.com>
2022-07-05 14:49:31 -0300

    primer push

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,14 @@
 /*
+Carolina Barresi 
+DIV X
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
-{
-	alert("ok");
+{// let asignar una variable local, no declarar su función en la misma linea
+	let nombre
+
+     nombre =prompt("Ingrese su nombre");
+     
+	alert("Su nombre es: " + nombre);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,9 +3,13 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	let nombreIngresado;
+	
+	nombreIngresado = document. getElementById("txtIdNombre").value;
+
+	//Funciona solo para Chrome, no sirve nombreIngresado=txtIdNombre.value;
+	
+	alert("Su nombre es " + nombreIngresado);
 
 }
 
